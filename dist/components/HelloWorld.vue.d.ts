1c3b12224ec840456bcf76361fd048b65ceedceb
LeWawan <erwankreutz@gmail.com>
2023-04-23 18:37:16 +0200

    refactor: remove dist files and update .gitignore

--- dist/components/HelloWorld.vue.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _sfc_main: import("vue").DefineComponent<{}, {
-    __sfc: boolean;
-    msg: import("vue").Ref<string>;
-}, {}, {}, {}, import("vue/types/v3-component-options").ComponentOptionsMixin, import("vue/types/v3-component-options").ComponentOptionsMixin, {}, string, Readonly<import("vue").ExtractPropTypes<{}>>, {}>;
-export default _sfc_main;
